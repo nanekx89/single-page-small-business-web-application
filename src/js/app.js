e858fe491016e4bd7e5947a2d874fc7b70375785
Reza Azimifar <azimifar.reza@gmail.com>
2024-04-30 20:27:14 +0330

    creation and styling of the navbar - part 4

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,30 @@
 
 $("#nav-toggle").on("click", function () {
     $("#nav-menu").toggleClass("show-menu");
-})
+});
+
+$(window).on("scroll", function () {
+    if (this.scrollY >= 100) {
+        $("#header").addClass("scroll-header");
+    } else {
+        $("#header").removeClass("scroll-header");
+    }
+
+    $("section").each(function (i) {
+        const sectionHeight = $("section")[i].offsetHeight;
+        const sectionTop = $("section")[i].offsetTop - 50;
+        const sectionId = $("section")[i].getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            $(`.nav__menu a[href*= ${sectionId}]`).addClass("active-link");
+        } else {
+            $(`.nav__menu a[href*= ${sectionId}]`).removeClass("active-link");
+        }
+    });
+});
+
+$(".nav__link").each(function (i) {
+    $(".nav__link").on("click", function () {
+        $("#nav-menu").removeClass("show-menu");
+    });
+});
